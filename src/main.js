78b74f4aac31b99e18b3481c72214eae23830412
marccharton <kramoogle@gmail.com>
2022-05-09 17:03:13 +0200

    ajout du détails des artistes

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const app = new App({
 		artists: [
 			{
 				name : 'Arthur Baude', 
-				nomination: "Designer Maker",
+				nomination: "Designer / Maker / VJ",
 				imgUrl: 'https://www.aht.li/3160494/avatar.jpg',
 				bio: "Issu d'une formation multimédia puis arts appliqués, j’ai découvert le milieu maker à la fin de mes études où j’ai pu me former en autodidacte à l'électronique interactive et à la fabrication numérique.Passionné par ces différents univers, je les intègre en permanence dans mon travail en mélangeant design, création artistique et fabrication numérique. Après 3 ans passé chez nod-A en tant que designer / maker / facilitateur, je travaille désormais en freelance. Depuis 2016, je consacre une grande part de mon temps professionnel à concevoir et donner des formations à la fabrication numérique et au monde maker dans sa globalité.",
 				social : [
@@ -18,14 +18,13 @@ const app = new App({
 				]
 			},
 			{
-				name : 'Makio135', 
+				name : 'Emiliano Germain', 
 				nomination: "Generative Artist",
-				imgUrl: 'https://images.squarespace-cdn.com/content/v1/5ed6306308e08b2bda7645bf/1617275774364-UWXUN83QQ8YC0NQBYY1N/QmYqaCmnERMTdEqnCkoeBtGcBE8DSzwBjbEvYs74vepGr1.gif?format=2500w',
+				imgUrl: 'https://i.imgur.com/Uvmb4jx.jpeg',
 				bio: "",
 				social : [
-					{name: "instagram", url:"https://twitter.com/arthur_baude"},
-
-			]
+					{name: "instagram", url:"https://www.grame.fr/artistes-chercheurs-partenaires/emiliano-germain"},
+				]
 			},
 			{
 				name : 'Romain Darracq', 
@@ -50,9 +49,9 @@ const app = new App({
 				]
 			},
 			{
-				name : 'Martial Geoffre-Rouland', 
-				nomination: "Studio Screen Club",
-				imgUrl: 'https://avatars.githubusercontent.com/u/157709?v=4',
+				name : 'VFG', 
+				nomination: "Live Digital Artist",
+				imgUrl: "",
 				bio: "",
 				social : [
 					{name: "instagram", url:"https://twitter.com/arthur_baude"},
@@ -60,6 +59,24 @@ const app = new App({
 					{name: "twitch", url:"https://twitter.com/arthur_baude"}
 				]
 			},
+			{
+				name : 'Arthur Saunier', 
+				nomination: "Instrument Maker",
+				imgUrl: 'https://scontent-mrs2-1.xx.fbcdn.net/v/t1.6435-9/71217672_2431888100260626_644484465812307968_n.jpg?_nc_cat=111&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=_336yh1TnAQAX_YlzsI&_nc_ht=scontent-mrs2-1.xx&oh=00_AT9YqWJNESzDBDhp8_hRwOCFaice2PbxoYeA3LqN1m_nSA&oe=629DF91C',
+				bio: "",
+				social : [
+					{name: "instagram", url:"https://github.com/arthursaunier/Synth"},
+				]
+			},
+			{
+				name : 'Pierre - WSK', 
+				nomination: "VJ / Scénographe",
+				imgUrl: 'https://visuaal.fr/2017/wordpress/wp-content/uploads/2017/02/WSK-AVE-w-Dunes-@-RUMBLE-Festival-2014-print23-1440x810.jpg',
+				bio: "",
+				social : [
+					{name: "instagram", url:"https://www.wskvj.com/"}
+				]
+			}
 		]
 	}
 });
